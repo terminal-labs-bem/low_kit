f98fbacc90c900173a35ba1643f32998b51e374e
user <user@workstation>
2024-03-31 01:30:50 -0500

    added in filesystem

--- src/lowkit/ops/filesystem.py
+++ src/lowkit/ops/filesystem.py
@@ -1,7 +1,11 @@
+import grp
+import pwd
 import os
 import sys
+import hashlib
 import tempfile
-from pathlib import Path
+from contextlib import contextmanager
+from pathlib import Path, PurePath
 from shutil import copyfile, move, rmtree
 
 def file_hash(file):
@@ -52,3 +56,52 @@ def file_rename(src, dst):
 
 def file_move(src, dst):
     move(src, dst)
+    
+def _get_pathfrom(fullpath, dirfrom):
+    dirfrom = dirfrom + "/"
+    return str(fullpath.replace(dirfrom,""))
+
+@contextmanager
+def contextmanager_cwd(path):
+    oldpwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(oldpwd)
+
+class FileObject:
+    def __init__(self, path , dirfrom):
+        self.fullpath = str(Path(path).absolute())       
+        self.dirfrom = str(Path(dirfrom).absolute())
+        self.pathfrom = _get_pathfrom(self.fullpath, self.dirfrom)
+        self.parent = str(PurePath(self.fullpath).parent)
+        self.suffix = str(PurePath(self.fullpath).suffix)
+        self.name = str(PurePath(self.fullpath).name)
+        self.stem  = str(PurePath(self.fullpath).stem)
+        self.size  = str(os.path.getsize(self.fullpath))
+        self.md5  = str(hashlib.md5(open(self.fullpath,'rb').read()).hexdigest())
+        self.statinfo  = str(os.stat(self.fullpath))
+        self.permissionmask  = str(oct(os.stat(self.fullpath).st_mode)[-3:])
+        self.owner  = str(Path(self.fullpath).owner())
+        self.group  = str(Path(self.fullpath).group())
+
+class FilesystemSingleton:
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(FilesystemSingleton, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def __init__(self):
+        pass
+ 
+    def getdirs(self, path):
+        complete_files = []
+        for root, dir_names, file_names in os.walk(path):
+            for f in file_names:
+                complete_files.append(os.path.join(root, f))
+        complete_files = sorted(complete_files)
+        return complete_files
+
+fs = FilesystemSingleton()
